8de22dbafcc41162778f11d977f42b3cf3e466bf
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2024-06-03 22:00:58 +0200

    Angular Directives

---
+++ JSAngular/AngularDirectives.js
@@ -0,0 +1,6 @@
+let app = angular.module("myApp", []);
+app.directive("myDirective", function(){
+    return{
+        template:"Directive data"
+    };
+})
---
+++ JSAngular/AngularModulesDirectivesAndControllers.js
@@ -0,0 +1,5 @@
+let app = angular.module("myApp", []);
+app.controller("myController", function($scope){
+    $scope.firstname = "Fernando";
+    $scope.lastname = "Paricio";
+});
